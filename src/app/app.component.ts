bb58282f26a3f6940391099daf461e26af40b30f
IBM931-PC0BQ513\Manish <manisk08@in.ibm.com>
2019-09-23 15:09:54 +0530

    More About @Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { Component } from "@angular/core";
 //@Component is Decorater for Appcomponent
 //Get new class wirth addtional feteres added by decorater
 @Component({
+  //3.where it needs to be rendered
   selector: "app-root",
+  //1.what needs to be rendered in Ui
   templateUrl: "./app.component.html",
+  //2.How it needs to be rendered in UI menas Style
   styleUrls: ["./app.component.css"]
+  //style ulr is an array, will be applied as in oreder ar mentioned
+  //customised for this component only
 })
+
+//Compoent is peace of UI
 export class AppComponent {
   title = "day01";
 }
