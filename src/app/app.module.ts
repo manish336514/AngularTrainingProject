5711ceab7ea564c714d290e553b5249d25057c2e
IBM931-PC0BQ513\Manish <manisk08@in.ibm.com>
2019-09-23 15:01:27 +0530

    Description of NG Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  //attribures: value
+
+  declarations: [AppComponent],
+
+  //other modules to be imported
   imports: [
     BrowserModule
+    //display of Ui in browser
   ],
+  //provider related to provide services (angular services )
+  //dependeniy injection done by provider
   providers: [],
+
+  //Boostarping of Application nothing to do with bootstarap
   bootstrap: [AppComponent]
+
+  //App componet in rrot component , all ui will be embeded in Appcomponent
+  //boostrap is an array, eager Loding =resources will be laded at application start up itself, occuping memory disad, adv, resouce is avilable
+  //lazy loding when needed then to load
+  //[]in bootstap all comp will be eager loded and Appcompoent will be root compoonent.
+  //order is important
+  //bbostarp is for eager loding
 })
-export class AppModule { }
+export class AppModule {}
