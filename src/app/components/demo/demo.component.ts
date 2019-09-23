79b7e309a94b4ff8c4ea5fc073f206ed4eadc629
IBM931-PC0BQ513\Manish <manisk08@in.ibm.com>
2019-09-23 15:56:55 +0530

    *ngFor Usage

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from "@angular/core";
 })
 export class DemoComponent implements OnInit {
   message;
+  vegetables;
   //or message:string
-  constructor() {
-    this.message = "Hello World";
-  }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log("ngOnInit called...");
+    this.message = "Hello World";
+    this.vegetables = [
+      { name: "Brinjal", price: 14 },
+      { name: "Bottle Ground", price: 25 },
+      { name: "Tomato", price: 15 },
+      { name: "Patato", price: 25 }
+    ];
+  }
 }
