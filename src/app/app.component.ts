c7982e8e2b486217e72b3064fffab0bcee53a170
IBM931-PC0BQ513\Manish <manisk08@in.ibm.com>
2019-09-23 15:05:18 +0530

    App component with deocrater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+//Decorated with @Component
+//@Component is Decorater for Appcomponent
+//Get new class wirth addtional feteres added by decorater
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'day01';
+  title = "day01";
 }
